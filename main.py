bb68191426f7febde1c46c9ac5adc88e5444ec78
NosiOi <yaroslavfedorak64@gmail.com>
2026-01-10 19:47:42 +0100

    оновлення

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from bot.config import BOT_TOKEN
 from bot.handlers.start import router as start_router
+from bot.handlers.menu import router as menu_router
 
 print("REDEPLOY TEST")
 print("ENV BOT_TOKEN:", os.getenv("BOT_TOKEN"))
@@ -14,7 +15,7 @@ async def main():
     dp = Dispatcher()
 
     dp.include_router(start_router)
-
+    dp.include_router(menu_router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
